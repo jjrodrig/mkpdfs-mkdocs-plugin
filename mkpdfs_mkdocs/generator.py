a9b6afbe7a547856e9255bc603e45492a7b60963
Juanjo Rodriguez <jjrodrig@gmail.com>
2020-07-10 18:39:37 +0200

    Update generator.py

--- mkpdfs_mkdocs/generator.py
+++ mkpdfs_mkdocs/generator.py
@@ -180,7 +180,7 @@ class Generator(object):
 
     def _gen_toc_section(self, section):
         for p in section.children:
-            if not p.is_page or not hasattr(p, 'meta')
+            if not p.is_page or not hasattr(p, 'meta'):
                 continue
             if not p.is_page and hasattr(p, 'meta') and p.meta != None and 'pdf' \
             in p.meta and p.meta['pdf'] == False:
